c9d6176197f7c756e76266da4e7f41b751ff9a01
RUDRA KANTI PARIA <154822292+RudraKantiParia@users.noreply.github.com>
2025-12-16 17:42:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from io import BytesIO
 
 st.set_page_config(page_title="LMS Timestamp Remover")
 
@@ -25,8 +26,8 @@ if uploaded_file:
     st.success("Timestamps removed successfully")
 
     st.download_button(
-        label="Download cleaned Excel file",
-        data=clean_df.to_excel(index=False),
-        file_name="lms_report_without_timestamps.xlsx",
-        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )
+    label="Download cleaned Excel file",
+    data=buffer,
+    file_name="lms_report_without_timestamps.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+)
